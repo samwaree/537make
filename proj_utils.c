a5b9234323f8805f3f298d72d2819633195aac73
Logan Mahan <mahan@snares-07.cs.wisc.edu>
2018-11-04 13:13:18 -0600

    Proj util update

--- proj_utils.c
+++ proj_utils.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "build_spec_repr.h"
+#include "linked_list.h"
 
 /*
  * MallocCheck evaluates pointers to make sure they were properly allocated.
  */
-void* mallocCheck(void* ptr){
+void mallocCheck(void* ptr){
 	if(ptr == NULL){
-		fprint("Error: Memory not allocated.");
+		printf("Error: Memory not allocated.");
 		exit(-1);
 	}
 }
 
-
+/*
+ * Finds and returns the Buildspec with the given target
+ * Returns NULL if none found
+ */
+Buildspec* find(Node* bs_list, char* target) {
+    for (int i = 0; i < size(bs_list); i++) {
+        Buildspec* bs = getElement(bs_list, i);
+        if (strcmp(target, getTarget(bs)) == 0) {
+            return bs;
+        }
+    }
+    return NULL;
+}
